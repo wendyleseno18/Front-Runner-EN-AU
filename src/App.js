0bb38eb479cff733c7e89b177cab7ff6a33256a9
wendyleseno18 <wendyle.seno@yotpo.com>
2025-07-28 14:05:37 +0800

    Remove overall prod id

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "287-500056P";
+  let ProductId = "";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
